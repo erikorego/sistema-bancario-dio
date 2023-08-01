35c012fe875024b9bcd4bb52a96cb06d27638c53
erikorego <erikorego@outlook.com>
2023-08-01 13:42:54 -0300

    Operação depósito implementada. Melhoria no armazenamento de informações na string Extrato.

--- sistema_bancario.py
+++ sistema_bancario.py
@@ -17,7 +17,24 @@ while True:
         valor = float(input("Insira o valor que deseja sacar: "))
         if valor <= saldo:
             saldo -= valor
-            extrato += f"R$ {valor}. Saldo {saldo}\n"
+            extrato += f"SAQUE: R$ {valor}. Saldo {saldo}\n"
             print("A operação foi realizada com sucesso")
         else:
             print("Saldo insuficiente!")
+    if opcao == 2:
+        valor_deposito = float(input("Insira o valor que deseja depositar: "))
+        if valor_deposito < 0:
+            print("""
+                  Você não pode depositar um valor negativo!
+                  Operação cancelada!
+""")
+        elif valor_deposito == 0:
+            print('''
+                  Operação Inválida!
+                  Você não pode depositar 0 reais
+                  Operação cancelada!
+''')
+        else:
+            saldo += valor_deposito
+            extrato += f"DEPÓSITO: R$ {valor}. Saldo {saldo}\n"
+            print("Operação realizada com sucesso!")
